976a0f0b64a8c69a17b03a0d79ceb60b9c3c6419
michaelkeates <mail@michaelkeates.co.uk>
2023-10-02 00:23:29 +0100

    Update secrets.nix

--- secrets.nix
+++ secrets.nix
@@ -2,7 +2,7 @@ let
   user1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFqmPf1+Z6kckWMBMDPHOmOMoHzCWW5J1uyyoOb5/Jgk";
   users = [ user1 ];
 
-  system1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHmIKZIpWabKSpqwU7r+ShTV9Vw26d/qV/R1tTnZiMHU";
+  system1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL/i4LlEidL5/CTmX1L+GzqswuR/3el+tuAdC4/JK9nS";
   systems = [ system1 ];
 in
 {
