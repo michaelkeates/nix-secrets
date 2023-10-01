d1cb4215dd3d3cc9a59804bca551925d69b35a6a
michaelkeates <mail@michaelkeates.co.uk>
2023-10-01 22:27:31 +0100

    Update secrets.nix

--- secrets.nix
+++ secrets.nix
@@ -1,8 +1,8 @@
 let
   system1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFqmPf1+Z6kckWMBMDPHOmOMoHzCWW5J1uyyoOb5/Jgk";
   system2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHmIKZIpWabKSpqwU7r+ShTV9Vw26d/qV/R1tTnZiMHU";
-  systems = [ system1, system2 ];
+  systems = [ system1 system2 ];
 in
 {
-  "secret.age".publicKeys = [ system1, system2 ];
+  "secret.age".publicKeys = [ system1 system2 ];
 }
