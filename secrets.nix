73c0eec7e0b4b95f3a4a30118f82de19201592a1
michaelkeates <mail@michaelkeates.co.uk>
2023-10-29 22:49:18 +0000

    Update secrets.nix

--- secrets.nix
+++ secrets.nix
@@ -1,9 +1,9 @@
 let
   system1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL/i4LlEidL5/CTmX1L+GzqswuR/3el+tuAdC4/JK9nS";
   system2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGTspawJiAnEvLCIRqsPh4cpcUi/srJp3+Zj1ZLIeLEv";
-  system3 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBJQI/hPchLZs8xVkAPVTE4kK+mpAZ7LBRTAAHIDl3gA";
+  system3 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBvRJAUfWwvBBevXaWocawnwMZ09ornbuR76TO+YlVoE";
   systems = [ system1 system2 system3 ];
 in
 {
-  "secret.age".publicKeys = systems;
+  "bitwarden-masterpassword.age".publicKeys = systems;
 }
