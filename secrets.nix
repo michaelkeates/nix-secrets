ffdd0571f8b4356ce12704c9108dbe455416790e
michaelkeates <103878560+michaelkeates@users.noreply.github.com>
2023-11-04 12:12:41 +0000

    Update secrets.nix

--- secrets.nix
+++ secrets.nix
@@ -1,7 +1,7 @@
 let
   system1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL/i4LlEidL5/CTmX1L+GzqswuR/3el+tuAdC4/JK9nS";
   system2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE4olXzuebhdlCClLi7m1JWRM4X9iLYPBTzX6AghGTJk";
-  system3 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBvRJAUfWwvBBevXaWocawnwMZ09ornbuR76TO+YlVoE";
+  system3 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPLOibLJ4wbkv3twIqI1VSCBgGBvr1FcjMjNmitt4cph";
   systems = [ system1 system2 system3 ];
 in
 {
