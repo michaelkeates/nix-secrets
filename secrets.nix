6f5334c415bf7681cd315f61e761484f8354ec10
michaelkeates <103878560+michaelkeates@users.noreply.github.com>
2023-11-04 17:06:16 +0000

    Update secrets.nix

--- secrets.nix
+++ secrets.nix
@@ -1,6 +1,6 @@
 let
   system1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFl8xlUvLRntYtX3uOB6sj8pwpp58w9xqKDsn8pDaN5Z";
-  systems = [ system1 system2 system3 ];
+  systems = [ system1 ];
 in
 {
   "bitwarden-masterpassword.age".publicKeys = systems;
